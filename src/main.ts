0fcbe3cdf7922c065a43378a357b3e74f9595f85
hannahellis4242 <hannahellis4242@gmail.com>
2022-04-09 08:33:58 +0100

    Adding a testing script and checking it can be read in

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import { readFile } from "fs";
+console.log(
+  "This first version will expect a file called script.json and produce a file called script.lsl"
+);
+
+readFile("script.json", (err, data) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(data.toString());
+  }
+});
