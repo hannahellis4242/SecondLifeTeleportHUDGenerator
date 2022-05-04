a4d9add3ed61c881f49cefb7f7a97af5078c4228
hannahellis4242 <hannahellis4242@gmail.com>
2022-05-05 00:47:44 +0100

    Adding styles

--- my-app/src/components/Output.tsx
+++ my-app/src/components/Output.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useRef } from "react";
 import createScript from "../script/createScript";
 import { ModelContext } from "../store/ModelContext";
+import classes from "./Output.module.css";
 
 const Output: React.FC = () => {
   const modelContext = useContext(ModelContext);
@@ -12,12 +13,27 @@ const Output: React.FC = () => {
       out.innerText = createScript(modelContext.topMenu);
     }
   };
+
+  const copyText = () => {
+    const out = output.current;
+    if (out) {
+      navigator.clipboard
+        .writeText(out.innerText)
+        .then(() => alert("seed copied to clipboard"))
+        .catch(() => alert("could not copy"));
+    }
+  };
   return (
-    <section>
-      <div>
-        <button onClick={generate}>Generate</button>
-      </div>
-      <p ref={output}></p>
+    <section className={classes.output}>
+      <header className={classes.head}>
+        <button className={classes.generate} onClick={generate}>
+          Generate
+        </button>
+        <button className={classes.copy} onClick={copyText}>
+          copy
+        </button>
+      </header>
+      <p className={classes.output_text} ref={output}></p>
     </section>
   );
 };
