151b0c844e5424bad9cc6422703e7ebaf71ad9b6
hannahellis4242 <hannahellis4242@gmail.com>
2022-05-05 12:52:58 +0100

    Adding a little styling

--- my-app/src/components/RectOption.tsx
+++ my-app/src/components/RectOption.tsx
@@ -2,30 +2,29 @@ import React from "react";
 import Action from "../model/Action";
 import Rectangle from "../model/Rectangle";
 import ActionTag from "./ActionTag";
+import classes from "./RectangleOption.module.css";
 
 const RectOption: React.FC<{ rect: Rectangle; action: Action }> = ({
   rect,
   action,
 }) => {
   return (
-    <section>
-      <section>
-        <header>Rectangle</header>
-        <table>
-          <th>
-            <td>Left</td>
-            <td>Bottom</td>
-            <td>Right</td>
-            <td>Top</td>
-          </th>
-          <tr>
-            <td>{rect.left}</td>
-            <td>{rect.bottom}</td>
-            <td>{rect.right}</td>
-            <td>{rect.top}</td>
-          </tr>
-        </table>
-      </section>
+    <section className={classes.option}>
+      <header className={classes.head}>Rectangle</header>
+      <table>
+        <tr>
+          <th>Left</th>
+          <th>Bottom</th>
+          <th>Right</th>
+          <th>Top</th>
+        </tr>
+        <tr>
+          <td>{rect.left}</td>
+          <td>{rect.bottom}</td>
+          <td>{rect.right}</td>
+          <td>{rect.top}</td>
+        </tr>
+      </table>
       <ActionTag value={action} />
     </section>
   );
