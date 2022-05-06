cdc904bc1152ab48b71bbe3bd719784b95eae5d0
hannahellis4242 <hannahellis4242@gmail.com>
2022-05-06 09:29:08 +0100

    Adding some file io to save config

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import FileIO from "./components/FileIO";
 import MenuTag from "./components/MenuTag";
 import Output from "./components/Output";
 import Raw from "./components/Raw";
@@ -8,6 +9,7 @@ const App: React.FC = () => {
   const modelContext = useContext(ModelContext);
   return (
     <div>
+      <FileIO />
       <MenuTag value={modelContext.topMenu} />
       <Raw />
       <Output />
---
+++ my-app/src/components/FileIO.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FileIO: React.FC = () => {
+  return (
+    <section>
+      <button>Save Config</button>
+      <button>Load Config</button>
+    </section>
+  );
+};
+
+export default FileIO;
