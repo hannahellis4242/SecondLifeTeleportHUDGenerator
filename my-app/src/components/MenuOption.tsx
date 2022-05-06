0ec4274dbf586449591dfed03bf5dd74f7ffb696
hannahellis4242 <hannahellis4242@gmail.com>
2022-05-06 09:23:52 +0100

    Adding some styling to options

--- my-app/src/components/MenuOption.tsx
+++ my-app/src/components/MenuOption.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Action from "../model/Action";
 import ActionTag from "./ActionTag";
+import classes from "./MenuOption.module.css";
 
 const MenuOption: React.FC<{ label: string; action: Action }> = ({
   label,
   action,
 }) => (
-  <section>
-    <header>{label}</header>
+  <section className={classes.option}>
+    <header className={classes.head}>Menu Option Label : {label}</header>
     <ActionTag value={action} />
   </section>
 );
