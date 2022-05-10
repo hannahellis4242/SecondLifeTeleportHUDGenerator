30e6317cf456d848a7be4d970e54ec89e318ba89
hannahellis4242 <hannahellis4242@gmail.com>
2022-05-10 22:40:05 +0100

    Wip

--- my-app/src/components/EditMenuTag.tsx
+++ my-app/src/components/EditMenuTag.tsx
@@ -1,28 +1,23 @@
 import React from "react";
 import Menu from "../model/Menu";
-import OptionTag from "./OptionTag";
-import classes from "./MenuTag.module.css";
-import AddTeleportOption from "./AddTeleportOption";
-import AddMenuOption from "./AddMenuOption";
+import classes from "./EditMenuTag.module.css";
+import EditOption from "./EditOption";
 
-const MenuTag: React.FC<{ value: Menu }> = ({ value }) => {
+const EditMenuTag: React.FC<{ value: Menu }> = ({ value }) => {
   const isTopLevel = !value.parentId;
   return (
     <section className={classes.menu}>
       <header className={isTopLevel ? classes.top_level : classes.head}>
         {isTopLevel ? <div>Top</div> : null}
         <div>Menu ID : {value.id}</div>
+        <button>Add</button>
       </header>
       {value.message ? <p>Message : {value.message} </p> : null}
       {value.options.map((option, index) => (
-        <OptionTag key={index} value={option} />
+        <EditOption key={index} value={option} />
       ))}
-      <section className={classes.controls}>
-        <AddTeleportOption menuID={value.id} top={isTopLevel} />
-        <AddMenuOption menuID={value.id} top={isTopLevel} />
-      </section>
     </section>
   );
 };
 
-export default MenuTag;
+export default EditMenuTag;
