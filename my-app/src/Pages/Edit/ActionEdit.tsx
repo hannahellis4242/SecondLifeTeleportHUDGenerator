8ad77449e4e3f6262c9f9968b1abe58dcdfd16b2
hannahellis4242 <hannahellis4242@gmail.com>
2022-05-15 15:34:45 +0100

    Small change to ActionEdit. We don't edit menu actions within other menu edits, so we can remove

--- my-app/src/Pages/Edit/ActionEdit.tsx
+++ my-app/src/Pages/Edit/ActionEdit.tsx
@@ -1,13 +1,12 @@
 import { FunctionComponent } from "react";
 import Action from "../../model/Action";
-import MenuTag from "./MenuTag";
 import Teleport from "./Teleport";
 
 const ActionEdit: FunctionComponent<{ value: Action }> = ({ value }) => {
   if (value.destination) {
     return <Teleport destination={value.destination} />;
   } else if (value.menu) {
-    return <MenuTag value={value.menu} />;
+    return <div>Cannot Edit A menu within another menu</div>;
   } else {
     return <div>!!!Invalid Action!!!</div>;
   }
